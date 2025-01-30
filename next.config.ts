760bcf7301e768c1b7e128f365a2080a40fa91dd
thewoowon <thewoowon@naver.com>
2025-01-31 00:48:09 +0900

    feat: next config webpack edited

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; // three.js 사용 시 필요할 수 있음
+    return config;
+  },
 };
 
 export default nextConfig;
