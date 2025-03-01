3bd811122996fab6f5e15af0dc8d2f700c0ba8b3
thewoowon <thewoowon@naver.com>
2025-03-01 13:51:51 +0900

    feat: next config edited

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   /* config options here */
   webpack: (config) => {
     config.resolve.fallback = { fs: false }; // three.js 사용 시 필요할 수 있음
     config.module.rules.push({
-      test: /\.glsl$/,
+      test: /\.(glsl|vs|fs)$/,
       use: "raw-loader",
     });
+
+    config.resolve.extensions.push(".glsl", ".vs", ".fs");
+
     return config;
   },
 };
