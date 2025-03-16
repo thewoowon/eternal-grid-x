80e42b1a6a6d2d066ed17129e7fed3d101b35e30
thewoowon <thewoowon@naver.com>
2025-03-16 14:27:19 +0900

    feat: better particles scene code

--- components/module/ParticlesScene/ParticlesScene.tsx
+++ components/module/ParticlesScene/ParticlesScene.tsx
@@ -8,25 +8,22 @@ import particlesVertexShader from "./shaders/particles/vertex.glsl";
 import particlesFragmentShader from "./shaders/particles/fragment.glsl";
 
 const DisplacementCanvas = () => {
-  const canvasRef = useRef(null);
-  const textureRef = useRef(
-    new THREE.CanvasTexture(document.createElement("canvas"))
-  );
+  const canvasRef = useRef<HTMLCanvasElement>(document.createElement("canvas"));
+  const textureRef = useRef(new THREE.CanvasTexture(canvasRef.current));
   const [canvasCursor, setCanvasCursor] = useState(
     new THREE.Vector2(9999, 9999)
   );
-  const glowImage = new Image();
-  glowImage.src = "/images/static/particles/glow.png";
+  const glowImage = useRef(new Image());
 
   useEffect(() => {
-    const canvas = canvasRef.current as HTMLCanvasElement | null;
-    if (!canvas) return;
+    glowImage.current.src = "/images/static/particles/glow.png";
+    const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+    if (!context) return;
+
     canvas.width = 128;
     canvas.height = 128;
-    if (context) {
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    }
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     const handlePointerMove = (event: PointerEvent) => {
       const rect = canvas.getBoundingClientRect();
@@ -40,25 +37,24 @@ const DisplacementCanvas = () => {
   }, []);
 
   useFrame(() => {
-    const canvas = canvasRef.current as HTMLCanvasElement | null;
-    if (!canvas) return;
+    const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    if (context) {
-      context.globalCompositeOperation = "source-over";
-      context.globalAlpha = 0.02;
-      context.fillRect(0, 0, canvas.width, canvas.height);
+    if (!context) return;
 
-      const glowSize = canvas.width * 0.25;
-      context.globalCompositeOperation = "lighten";
-      context.globalAlpha = 0.5;
-      context.drawImage(
-        glowImage,
-        canvasCursor.x - glowSize * 0.5,
-        canvasCursor.y - glowSize * 0.5,
-        glowSize,
-        glowSize
-      );
-    }
+    context.globalCompositeOperation = "source-over";
+    context.globalAlpha = 0.02;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const glowSize = canvas.width * 0.25;
+    context.globalCompositeOperation = "lighten";
+    context.globalAlpha = 0.5;
+    context.drawImage(
+      glowImage.current,
+      canvasCursor.x - glowSize * 0.5,
+      canvasCursor.y - glowSize * 0.5,
+      glowSize,
+      glowSize
+    );
 
     textureRef.current.needsUpdate = true;
   });
@@ -71,36 +67,35 @@ const Particles = () => {
   const pictureTexture = useTexture("/images/static/particles/picture-1.png");
   const displacementTexture = DisplacementCanvas();
 
-  const geometryRef = useRef<THREE.PlaneGeometry>(null);
-  const materialRef = useRef<THREE.ShaderMaterial>(null);
+  const geometryRef = useRef(new THREE.PlaneGeometry(10, 10, 128, 128));
 
   useEffect(() => {
-    const geometry = new THREE.PlaneGeometry(10, 10, 128, 128);
     const intensitiesArray = new Float32Array(
-      geometry.attributes.position.count
+      geometryRef.current.attributes.position.count
     );
-    const anglesArray = new Float32Array(geometry.attributes.position.count);
+    const anglesArray = new Float32Array(
+      geometryRef.current.attributes.position.count
+    );
 
-    for (let i = 0; i < geometry.attributes.position.count; i++) {
+    for (let i = 0; i < geometryRef.current.attributes.position.count; i++) {
       intensitiesArray[i] = Math.random();
       anglesArray[i] = Math.random() * Math.PI * 2;
     }
 
-    geometry.setAttribute(
+    geometryRef.current.setAttribute(
       "aIntensity",
       new THREE.BufferAttribute(intensitiesArray, 1)
     );
-    geometry.setAttribute("aAngle", new THREE.BufferAttribute(anglesArray, 1));
-    geometryRef.current = geometry;
+    geometryRef.current.setAttribute(
+      "aAngle",
+      new THREE.BufferAttribute(anglesArray, 1)
+    );
   }, []);
-
-  if (!geometryRef.current || !materialRef.current) return null;
 
   return (
     <points>
       <primitive object={geometryRef.current} />
       <shaderMaterial
-        ref={materialRef}
         vertexShader={particlesVertexShader}
         fragmentShader={particlesFragmentShader}
         uniforms={{
